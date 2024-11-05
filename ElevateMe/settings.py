7d28facbfcafa789261d5a7109623f27be1b0302
DodingLetou <144013045+ErwinLambujon@users.noreply.github.com>
2024-11-06 01:19:14 +0800

    Update settings.py

--- ElevateMe/settings.py
+++ ElevateMe/settings.py
@@ -144,6 +144,11 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 AUTH_USER_MODEL = 'api.CustomUser'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
